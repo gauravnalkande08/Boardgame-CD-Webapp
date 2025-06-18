acda0cbe118294a7f59b0d95e5fc9b8046bd65b5
gauravnalkande08 <140478777+gauravnalkande08@users.noreply.github.com>
2025-06-18 17:39:35 +0530

    Update variable.tf

--- modules/webapp/variable.tf
+++ modules/webapp/variable.tf
@@ -1,35 +1,73 @@
-variable "name" {
-  description = "The name of the Azure Web App"
+variable "artifact_path" {
+  description = "The local file path to the ZIP artifact for deployment. Set to null if not deploying via this method."
+  type        = string
+  default     = null # Default to null, making it optional for cases where you don't use this deployment method.
+}
+
+# Your existing variables (examples below, ensure yours are correct):
+variable "webapp_name" {
+  description = "Name of the Azure Web App."
   type        = string
 }
 
 variable "location" {
-  description = "The Azure region for the Web App"
+  description = "Azure region where resources will be deployed."
   type        = string
 }
 
 variable "resource_group_name" {
-  description = "The name of the resource group where the Web App will be created"
+  description = "Name of the resource group where the Web App will reside."
   type        = string
 }
 
 variable "service_plan_id" {
-  description = "The ID of the App Service Plan to associate with the Web App"
+  description = "ID of the App Service Plan to associate with the Web App."
   type        = string
+}
+
+variable "minimum_tls_version" {
+  description = "Minimum TLS version for the Web App."
+  type        = string
+  default     = "1.2"
+}
+
+variable "technology" {
+  description = "Technology stack of the web app (e.g., 'dotnet', 'java', 'node', 'python')."
+  type        = string
+}
+
+variable "node_version" {
+  description = "Node.js version for the web app (if technology is 'node')."
+  type        = string
+  default     = null
 }
 
 variable "java_version" {
-  description = "The Java version for the Web App runtime"
+  description = "Java version for the web app (if technology is 'java')."
   type        = string
+  default     = null
 }
 
-variable "app_jar_name" {
-  description = "The name of your deployed JAR file (e.g., app.jar or your_artifact_name.jar)"
+variable "java_server" {
+  description = "Java server for the web app (e.g., 'TOMCAT', 'JBOSS')."
   type        = string
+  default     = null
 }
 
-variable "app_settings" {
-  description = "A map of application settings (environment variables) for the Web App"
-  type        = map(string)
-  default     = {}
+variable "java_server_version" {
+  description = "Java server version for the web app."
+  type        = string
+  default     = null
+}
+
+variable "python_version" {
+  description = "Python version for the web app (if technology is 'python')."
+  type        = string
+  default     = null
+}
+
+variable "dotnet_version" {
+  description = "Dotnet version for the web app (if technology is 'dotnet')."
+  type        = string
+  default     = null
 }
