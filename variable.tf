3cb6137cf23a3c3b3f5ea7746b65dd105821d6ee
gauravnalkande08 <140478777+gauravnalkande08@users.noreply.github.com>
2025-06-18 19:13:39 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -16,10 +16,10 @@ variable "jfrog_url" {}
 # export JFROG_USER="your_jfrog_username"
 # export JFROG_PASSWORD="your_jfrog_api_key_or_access_token"
 
-variable "dotnet_artifact_version" {
-  description = "The specific version of the Dotnet application artifact to download and deploy (e.g., '1.0.0' or 'build-123'). Changing this value will trigger a new download and deployment."
-  type        = string
-}
+# variable "dotnet_artifact_version" {
+#   description = "The specific version of the Dotnet application artifact to download and deploy (e.g., '1.0.0' or 'build-123'). Changing this value will trigger a new download and deployment."
+#   type        = string
+# }
 
 variable "java_artifact_version" {
   description = "The specific version of the Java application artifact to download and deploy (e.g., '1.0.0' or 'build-456'). Changing this value will trigger a new download and deployment."
