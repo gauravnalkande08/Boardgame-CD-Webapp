0c1046a9e40ad1f701538935ba44cd5aca896164
gauravnalkande08 <140478777+gauravnalkande08@users.noreply.github.com>
2025-06-18 14:11:02 +0530

    Update main.tf

--- modules/appServicePlan/main.tf
+++ modules/appServicePlan/main.tf
@@ -1,1 +1,7 @@
-
+resource "azurerm_service_plan" "main" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  os_type             = "Linux" # Essential for Java JAR deployments
+  sku_name            = var.sku_name
+}
