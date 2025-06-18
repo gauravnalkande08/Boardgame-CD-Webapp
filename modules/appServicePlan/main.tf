aa2d36002baadee47080a871ea45d70a47f78841
gauravnalkande08 <140478777+gauravnalkande08@users.noreply.github.com>
2025-06-18 18:11:59 +0530

    Update main.tf

--- modules/appServicePlan/main.tf
+++ modules/appServicePlan/main.tf
@@ -1,7 +1,12 @@
-resource "azurerm_service_plan" "main" {
-  name                = var.name
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = var.app_service_plan_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  os_type             = "Linux" # Essential for Java JAR deployments
-  sku_name            = var.sku_name
+  kind                = var.kind
+  reserved            = var.kind == "Linux" ? true : false # Required for Linux App Service Plans
+
+  sku {
+    tier = var.tier
+    size = var.size
+  }
 }
