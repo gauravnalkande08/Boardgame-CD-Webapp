a8f257850a467620a5c2062711d46e402a355cbf
gauravnalkande08 <140478777+gauravnalkande08@users.noreply.github.com>
2025-06-18 15:35:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 # Resource Group (remains in root as it's often a top-level dependency)
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
