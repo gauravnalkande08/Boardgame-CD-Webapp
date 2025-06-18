96dc80346860c5a478f3befd16652f91dd8bb755
gauravnalkande08 <140478777+gauravnalkande08@users.noreply.github.com>
2025-06-18 14:03:57 +0530

    Update output.tf

--- modules/webapp/output.tf
+++ modules/webapp/output.tf
@@ -1,1 +1,14 @@
+output "id" {
+  description = "The ID of the Azure Web App"
+  value       = azurerm_linux_web_app.main.id
+}
 
+output "name" {
+  description = "The name of the Azure Web App"
+  value       = azurerm_linux_web_app.main.name
+}
+
+output "default_hostname" {
+  description = "The default hostname of the Azure Web App"
+  value       = azurerm_linux_web_app.main.default_hostname
+}
