e77123f4bbef0676f7bcde7903056e3c5877d953
gauravnalkande08 <140478777+gauravnalkande08@users.noreply.github.com>
2025-06-18 17:59:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,26 +4,86 @@ resource "azurerm_resource_group" "main" {
   location = var.location
 }
 
-# Call the App Service Plan Module
-module "app_service_plan" {
-  source = "./modules/app-service-plan" # Local path to your module
-
-  name                = var.app_service_plan_name
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  sku_name            = var.app_service_plan_sku_name
+# App Service Plan (Assuming you have one or more for your web apps)
+module "appserviceplan1" {
+  source              = "./modules/appserviceplan" # Assuming you have this local module
+  resource_group_name = azurerm_resource_group.support_rg.name
+  location            = azurerm_resource_group.support_rg.location
+  app_service_plan_name = "kinog-appservice-plan-1"
+  tier                = "Standard"
+  size                = "S1"
+  kind                = "Linux" # For Linux web apps
+}
+##############################################################################################################
+locals {
+  dotnet_artifact_local_path = "${path.cwd}/artifacts/helloworld-dotnet-app-${var.dotnet_artifact_version}.zip"
+  java_artifact_local_path   = "${path.cwd}/artifacts/boardgame-java-app-${var.java_artifact_version}.zip"
 }
 
-# Call the Web App Module
-module "webapp" {
-  source = "./modules/webapp" # Local path to your module
+# Resource to download the Dotnet artifact from JFrog Artifactory
+resource "null_resource" "download_dotnet_artifact" {
+  triggers = {
+    artifact_version = var.dotnet_artifact_version
+  }
 
-  name                = var.webapp_name
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  service_plan_id     = module.app_service_plan.id # Output from the app_service_plan module
+  provisioner "local-exec" {
+    command = "mkdir -p ${path.cwd}/artifacts && curl -sSL -u \"${JFROG_USER}:${JFROG_PASSWORD}\" -o \"${local.dotnet_artifact_local_path}\" \"${var.jfrog_url}/my-repo/helloworld-dotnet-app-${var.dotnet_artifact_version}.zip\""
+  }
+  depends_on = [module.webappdotnet]
+}
 
-  java_version        = var.webapp_java_version
-  app_jar_name        = var.webapp_app_jar_name
-  app_settings        = var.webapp_app_settings
+# Resource to download the Java artifact from JFrog Artifactory
+resource "null_resource" "download_java_artifact" {
+  triggers = {
+    artifact_version = var.java_artifact_version
+  }
+
+  provisioner "local-exec" {
+    command = "mkdir -p ${path.cwd}/artifacts && curl -sSL -u \"${JFROG_USER}:${JFROG_PASSWORD}\" -o \"${local.java_artifact_local_path}\" \"${var.jfrog_url}/my-repo/boardgame-java-app-${var.java_artifact_version}.zip\""
+  }
+
+  depends_on = [module.webappjava]
+}
+
+# webapp dotnet kino ref app
+module "webappdotnet" {
+  source              = "./modules/webapp"
+  webapp_name         = "helloworld-webapp-dotnet-kino"
+  service_plan_id     = module.appserviceplan1.app_service_plan_id
+  location            = var.location
+  resource_group_name = var.support_resource_group
+  minimum_tls_version = "1.2"
+  technology          = "dotnet"
+  dotnet_version      = "6.0"
+  # NEW: Pass the local path to the downloaded artifact.
+  # The conditional 'null_resource.download_dotnet_artifact.id != ""' ensures
+  # that this path is only passed if the download resource has been successfully
+  # created/updated (i.e., the download completed).
+  artifact_path       = null_resource.download_dotnet_artifact.id != "" ? local.dotnet_artifact_local_path : null
+}
+
+# Output for Dotnet Web App hostname
+output "default_site_hostname_dotnet_app" {
+  value = module.webappdotnet.default_site_hostname
+}
+
+# webapp java kino ref app
+module "webappjava" {
+  source              = "./modules/webapp"
+  webapp_name         = "boardgame-webapp-java-kino"
+  service_plan_id     = module.appserviceplan1.app_service_plan_id
+  location            = var.location
+  resource_group_name = var.support_resource_group
+  minimum_tls_version = "1.2"
+  technology          = "java"
+  java_version        = "11"
+  java_server         = "TOMCAT"
+  java_server_version = "10.0"
+  # NEW: Pass the local path to the downloaded artifact.
+  artifact_path       = null_resource.download_java_artifact.id != "" ? local.java_artifact_local_path : null
+}
+
+# Output for Java Web App hostname
+output "default_site_hostname_java_app" {
+  value = module.webappjava.default_site_hostname
 }
