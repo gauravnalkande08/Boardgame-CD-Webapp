119cc1e471ca1fcd8afb456249f67989fb20ba93
gauravnalkande08 <140478777+gauravnalkande08@users.noreply.github.com>
2025-06-18 13:35:39 +0530

    Update variable.tf

--- modules/appServicePlan/variable.tf
+++ modules/appServicePlan/variable.tf
@@ -1,1 +1,21 @@
+# modules/app-service-plan/variables.tf
 
+variable "name" {
+  description = "The name of the App Service Plan"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region for the App Service Plan"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group where the App Service Plan will be created"
+  type        = string
+}
+
+variable "sku_name" {
+  description = "The SKU name for the App Service Plan (e.g., B1, S1, P1v2)"
+  type        = string
+}
