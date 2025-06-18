e7576e7c978df0584d52b4cf4a0d222a382e911d
gauravnalkande08 <140478777+gauravnalkande08@users.noreply.github.com>
2025-06-18 18:19:00 +0530

    Update variable.tf

--- modules/appServicePlan/variable.tf
+++ modules/appServicePlan/variable.tf
@@ -1,30 +1,5 @@
-variable "app_service_plan_name" {
-  description = "Name of the App Service Plan."
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "Name of the resource group for the App Service Plan."
-  type        = string
-}
-
-variable "location" {
-  description = "Azure region for the App Service Plan."
-  type        = string
-}
-
-variable "tier" {
-  description = "Tier of the App Service Plan (e.g., 'Standard', 'Basic', 'Premium')."
-  type        = string
-}
-
-variable "size" {
-  description = "Size of the App Service Plan (e.g., 'S1', 'B1', 'P1v2')."
-  type        = string
-}
-
-variable "kind" {
-  description = "Operating system for the App Service Plan ('Linux' or 'Windows')."
-  type        = string
-  default     = "Linux" # Assuming Linux based on previous web app config
-}
+variable "app_service_plan_name" {}
+variable "resource_group_name" {}
+variable "location" {}
+variable "os_type" {}
+variable "sku_name" {}
