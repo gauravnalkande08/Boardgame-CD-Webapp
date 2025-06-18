b8e6c5b40029ef14a5bb9dcdf4e61313d059e412
gauravnalkande08 <140478777+gauravnalkande08@users.noreply.github.com>
2025-06-18 14:10:22 +0530

    Update output.tf

--- modules/appServicePlan/output.tf
+++ modules/appServicePlan/output.tf
@@ -1,1 +1,9 @@
+output "id" {
+  description = "The ID of the App Service Plan"
+  value       = azurerm_service_plan.main.id
+}
 
+output "name" {
+  description = "The name of the App Service Plan"
+  value       = azurerm_service_plan.main.name
+}
