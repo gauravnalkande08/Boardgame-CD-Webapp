b35029f9b8f34c6f9cfcc5faa05318c4c8e4d4af
gauravnalkande08 <140478777+gauravnalkande08@users.noreply.github.com>
2025-06-20 12:02:53 +0530

    Update variable.tf

--- modules/appServicePlan/variable.tf
+++ modules/appServicePlan/variable.tf
@@ -1,5 +1,24 @@
-variable "app_service_plan_name" {}
-variable "resource_group_name" {}
-variable "location" {}
-variable "os_type" {}
-variable "sku_name" {}
+variable "app_service_plan_name" {
+  description = "Name of the App Service Plan."
+  type        = string # Added type
+}
+variable "resource_group_name" {
+  description = "Name of the resource group for the App Service Plan."
+  type        = string # Added type
+}
+variable "location" {
+  description = "Azure region for the App Service Plan."
+  type        = string # Added type
+}
+variable "os_type" {
+  description = "Operating system for the App Service Plan ('Linux' or 'Windows')."
+  type        = string # Added type
+}
+variable "sku_name" { # This will be the size (e.g., 'B1', 'S1')
+  description = "The name (size) of the SKU for the App Service Plan."
+  type        = string # Added type
+}
+variable "sku_tier" { # Added for clarity and to align with the 'sku' block in main.tf
+  description = "The tier of the SKU for the App Service Plan (e.g., 'Basic', 'Standard')."
+  type        = string # Added type
+}
