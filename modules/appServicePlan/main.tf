bc6fd10879ba7c9abf11c5cf351072d0e069cf04
gauravnalkande08 <140478777+gauravnalkande08@users.noreply.github.com>
2025-06-20 12:00:03 +0530

    Update main.tf

--- modules/appServicePlan/main.tf
+++ modules/appServicePlan/main.tf
@@ -1,7 +1,12 @@
-resource "azurerm_app_service_plan" "app_service_plan" {
+resource "azurerm_app_service_plan" "app_service_plan" { # Corrected resource name to "app_service_plan"
   name                = var.app_service_plan_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  os_type             = var.os_type
-  sku_name            = var.sku_name 
+  kind                = var.os_type # Uses var.os_type from module variable
+  reserved            = var.os_type == "Linux" ? true : false # Required for Linux App Service Plans
+
+  sku {
+    tier = var.sku_tier # Tier (e.g., "Basic", "Standard")
+    size = var.sku_name # Size (e.g., "B1", "S1")
+  }
 }
