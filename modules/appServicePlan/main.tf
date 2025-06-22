33df69c94e9895e0cb45c79d40447f008f50c2d4
gauravnalkande08 <140478777+gauravnalkande08@users.noreply.github.com>
2025-06-22 17:46:15 +0530

    Update main.tf

--- modules/appServicePlan/main.tf
+++ modules/appServicePlan/main.tf
@@ -2,8 +2,6 @@ resource "azurerm_service_plan" "app_service_plan" {
   name                = var.app_service_plan_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  sku {
-    tier = var.sku_tier
-    size = var.sku_size
-  }
+    name = var.sku_name
+    type = var.os_type
 }
