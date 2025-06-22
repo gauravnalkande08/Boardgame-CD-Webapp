45489cc9d98a074d984dd20b2e18d19db504a645
gauravnalkande08 <140478777+gauravnalkande08@users.noreply.github.com>
2025-06-22 17:23:44 +0530

    Update main.tf

--- modules/appServicePlan/main.tf
+++ modules/appServicePlan/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_app_service_plan" "app_service_plan" { # Corrected resource name to "app_service_plan"
+resource "azurerm_service_plan" "app_service_plan" { # Corrected resource name to "app_service_plan"
   name                = var.app_service_plan_name
   location            = var.location
   resource_group_name = var.resource_group_name
