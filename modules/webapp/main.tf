738d7f38235c9ee60769180fc3eac120089812bf
gauravnalkande08 <140478777+gauravnalkande08@users.noreply.github.com>
2025-06-25 13:58:31 +0530

    Update main.tf

--- modules/webapp/main.tf
+++ modules/webapp/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_linux_web_app" "webapp" { # Resource name is "webapp"
+resource "azurerm_linux_web_app" "webapp" {
   name                = var.webapp_name
   location            = var.location
   resource_group_name = var.resource_group_name
@@ -6,7 +6,9 @@ resource "azurerm_linux_web_app" "webapp" { # Resource name is "webapp"
   https_only          = true
 
   site_config {
+    always_on           = true
     minimum_tls_version = var.minimum_tls_version
+
     app_settings = {
       WEBSITE_RUN_FROM_PACKAGE = "1" # Tells App Service to run from the deployed zip
       # WEBSITES_PORT = "8080" # Uncomment if your app listens on this port
@@ -18,7 +20,7 @@ resource "azurerm_linux_web_app" "webapp" { # Resource name is "webapp"
       java_server_version = var.technology == "java" ? var.java_server_version : null
       python_version      = var.technology == "python" ? var.python_version : null
       dotnet_version      = var.technology == "dotnet" ? var.dotnet_version : null
-    app_command_line    = var.technology == "java" ? "java -jar /home/site/wwwroot/database_service_project-${var.java_artifact_version}-SNAPSHOT.jar.original --server.port=80" : null
     }
-   }
+    app_command_line    = var.technology == "java" ? "java -jar /home/site/wwwroot/database_service_project-${var.java_artifact_version}-SNAPSHOT.jar.original --server.port=80" : null
   }
+}
