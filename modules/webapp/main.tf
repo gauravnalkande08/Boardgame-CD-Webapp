901ae01f65c918a8409c27fe4cb2c2c6db0d4e88
gauravnalkande08 <140478777+gauravnalkande08@users.noreply.github.com>
2025-06-25 14:14:05 +0530

    Update main.tf

--- modules/webapp/main.tf
+++ modules/webapp/main.tf
@@ -1,3 +1,5 @@
+# modules/webapp/main.tf (This is the content you need to replace in that specific file)
+
 resource "azurerm_linux_web_app" "webapp" {
   name                = var.webapp_name
   location            = var.location
@@ -6,13 +8,15 @@ resource "azurerm_linux_web_app" "webapp" {
   https_only          = true
 
   site_config {
-    always_on           = true
+    always_on           = true # Recommended to keep 'always_on' for Java apps
     minimum_tls_version = var.minimum_tls_version
 
+    # app_settings MUST be inside the site_config block
     app_settings = {
-      WEBSITE_RUN_FROM_PACKAGE = "1" # Tells App Service to run from the deployed zip
+      WEBSITE_RUN_FROM_PACKAGE = "1"
       # WEBSITES_PORT = "8080" # Uncomment if your app listens on this port
     }
+
     application_stack {
       node_version        = var.technology == "node" ? var.node_version : null
       java_version        = var.technology == "java" ? var.java_version : null
@@ -20,7 +24,10 @@ resource "azurerm_linux_web_app" "webapp" {
       java_server_version = var.technology == "java" ? var.java_server_version : null
       python_version      = var.technology == "python" ? var.python_version : null
       dotnet_version      = var.technology == "dotnet" ? var.dotnet_version : null
+      linux_fx_version    = var.technology == "java" ? "JAVA|${var.java_version}" : null
     }
-    app_command_line    = var.technology == "java" ? "java -jar /home/site/wwwroot/database_service_project-${var.java_artifact_version}-SNAPSHOT.jar.original --server.port=80" : null
+
+    # app_command_line MUST be directly inside the site_config block, not inside application_stack
+    app_command_line = var.technology == "java" ? "java -jar /home/site/wwwroot/database_service_project-${var.java_artifact_version}-SNAPSHOT.jar.original --server.port=80" : null
   }
 }
