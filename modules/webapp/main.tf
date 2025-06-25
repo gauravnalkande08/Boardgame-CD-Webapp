fd8427df02cda09ed37deef5c9376cb98d3fc7bd
gauravnalkande08 <140478777+gauravnalkande08@users.noreply.github.com>
2025-06-25 15:21:42 +0530

    Update main.tf

--- modules/webapp/main.tf
+++ modules/webapp/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_linux_web_app" "webapp" {
       python_version      = var.technology == "python" ? var.python_version : null
       dotnet_version      = var.technology == "dotnet" ? var.dotnet_version : null
     }
-    app_command_line = var.technology == "java" ? "java -jar /home/site/wwwroot/database_service_project-${var.java_artifact_version}-SNAPSHOT.jar.original --server.port=80" : null
+    app_command_line = var.technology == "java" ? "java -jar /home/site/wwwroot/database_service_project-${var.java_artifact_version}-SNAPSHOT.zip --server.port=80" : null
   }
   
 # It takes the path to the local ZIP file.
